987e8fd1511575b961611926d68d2ced9dfd8b6b
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-10-13 23:51:19 +0100

    new folder

---
+++ PasswordChecker/index.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+const message = document.querySelector(",message")
+const show = document.querySelector(".show");
+
+
+
+let WeakPassword = /[a-z]/;
+let MediumPassword =/\d+/;
+let StrongPassword = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+
