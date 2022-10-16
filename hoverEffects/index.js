d437554c4b1bec45dc91a6d7522cef3178a54f79
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-10-16 12:29:27 +0100

    new addition

--- hoverEffects/index.js
+++ hoverEffects/index.js
@@ -4,4 +4,7 @@ document.body.addEventListener('mouseover', (e)=>{
    let x = ((window.innerWidth / 3) - e.pageX)/ 20
    let y = ((window.innerHeight / 3) - e.pageY) / 20 
    container.style.transform = `rotateX(${-y*2}deg) rotateY(${-x*2}deg) translateY(${x*2}px) translateX(${y*2}px)`
+   person.forEach.call(person, person =>{
+    person.style.transform = `translateY(${x}px) translateX(${y}px`;
+   })
 })
