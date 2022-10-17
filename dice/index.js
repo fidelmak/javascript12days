85eed97e47ea17e3e22fd5fbd543b99a1cc5e5cd
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-10-17 23:22:33 +0100

    added dice game

---
+++ dice/index.js
@@ -0,0 +1,26 @@
+const player0A = document.querySelector(".player_9")
+const player1A = document.querySelector(".player_1")
+
+const score0A = document.querySelector("score_0")
+const score1A = document.querySelector("score_1")
+
+const current0A = document.querySelector("current_0")
+const current1A = document.querySelector("current_1")
+
+const dice = document.querySelector(".dice")
+const btn_new = document.querySelector(".btn_new")
+const btn_hold = document.querySelector(".btn_hold")
+
+const btn_roll = document.querySelector("btn_roll")
+
+//initial state
+
+let scores, currentscore, activePlayer, game
+function initial_state (){
+    scores[0]
+    currentScore =0
+    activePlayer = 0
+    game = true;
+    
+
+}
