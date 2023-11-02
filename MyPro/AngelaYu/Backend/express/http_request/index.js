1bab698d2ae2f015d1dd6b262bb82378f3c4d905
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-11-02 17:54:53 +0100

    Changes to be committed:

---
+++ MyPro/AngelaYu/Backend/express/http_request/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  console.log(req.rawHeaders);
+  res.send("Hello my people ");
+});
+app.listen(port, () => {
+  console.log(`the server has started on ${port} `);
+});
