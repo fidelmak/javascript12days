409718e68f4b6afa348d51da08a878c90a7d645b
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-11-03 12:36:33 +0100

    Your branch is up to date with 'origin/master'.

--- MyPro/AngelaYu/Backend/express/http_request/index.js
+++ MyPro/AngelaYu/Backend/express/http_request/index.js
@@ -2,8 +2,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  console.log(req.rawHeaders);
-  res.send("Hello my people ");
+  //console.log(req.rawHeaders);
+  res.send(
+    "<b><center>This is the beginning of development <br /> <code> edit here </code></center></b> "
+  );
+});
+app.get("/about", (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send(
+    "<b><center> About our program here <br /> <button> click here </button></center></b> "
+  );
+});
+app.get("/contact", (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("<b><center> contact us here </center></b> ");
 });
 app.listen(port, () => {
   console.log(`the server has started on ${port} `);
--- MyPro/AngelaYu/introduction_js/index.js
+++ MyPro/AngelaYu/introduction_js/index.js
@@ -1,33 +1,19 @@
 //LEAP YEAR CHALLENGE
 function isLeap(year) {
-    
-    /**************Don't change the code above****************/    
-        
-        //Write your code here.
-    var leap = 4;
-     var divide = year % leap;    
-    if(divide === 0){
-        if(divide % 100 ===0){
-            if(divide % 400 === 0){
-                return "Leap year."
-    }else
-    { 
-        return 'not Leap year'
-        
+  var leap = 4;
+  var divide = year % leap;
+  if (divide === 0) {
+    if (divide % 100 === 0) {
+      if (divide % 400 === 0) {
+        return "Leap year.";
+      } else {
+        return "not Leap year";
+      }
+    } else {
+      return "not Leap year";
     }
-    }else
-    { 
-        return 'not Leap year'
-        
-    }
-    }else
-    { 
-        return 'not Leap year'
-        
-    }
-    
-        
-    
-    
-    }
-    console.log(isLeap(365))
+  } else {
+    return "not Leap year";
+  }
+}
+console.log(isLeap(365));
